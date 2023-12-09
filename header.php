f3ed4544b8af4d8a856ce454d9e91af802997672
Daemon507 <legacybronsels997@gmail.com>
2023-12-09 10:40:12 +0300

    First commit goes here 	new file:   chat.php 	new file:   header.php 	new file:   img.jpg 	new file:   index.php 	new file:   js/chat.js 	new file:   js/login.js 	new file:   js/pass_show_hide.js 	new file:   js/signup.js 	new file:   js/users.js 	new file:   login.php 	new file:   php/config.php 	new file:   php/data.php 	new file:   php/get-chat.php 	new file:   php/images/170060247420231024_1726121.jpg 	new file:   php/images/1700602978ColorPop1634203007869.jpeg 	new file:   php/images/1700603255photo_2023-05-24_20-14-05.jpg 	new file:   php/images/1700603457photo_2023-05-29_10-29-25.jpg 	new file:   php/insert-chat.php 	new file:   php/login.php 	new file:   php/logout.php 	new file:   php/search.php 	new file:   php/signup.php 	new file:   php/users.php 	new file:   style.css 	new file:   users.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>IChat</title>
+  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="../offlinecdns/boxicons/css/boxicons.css">
+  <link rel="stylesheet" href="../offlinecdns/boxicons/css/boxicons.min.css">
+</head>

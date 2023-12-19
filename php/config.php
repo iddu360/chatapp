c1cb69b6a64988d0964b110e4228f2d87dc5d51a
Daemon507 <legacybronsels997@gmail.com>
2023-12-19 11:32:22 +0300

    Second one. Figuring things out

--- php/config.php
+++ php/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn=mysqli_connect("localhost","root","lonewolffe","chat");
+$conn=mysqli_connect("localhost","root","lonewolffe","chat_db");
 if(!$conn){
   echo "Database connected" . mysqli_connect_error();
 }
